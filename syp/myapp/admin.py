84b8af29233d2b8612e8974f7b0be4c1d5f931b1
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-20 19:53:24 -0700

    Update admin.py

--- syp/myapp/admin.py
+++ syp/myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import MusicalEvent
+from .models import MusicalEvent, OurProduct,Feedback
 
 admin.site.register(MusicalEvent)
+admin.site.register(OurProduct)
+admin.site.register(Feedback)
