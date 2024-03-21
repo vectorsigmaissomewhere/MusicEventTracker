013eacff62391e2c6edc22d2dcf93c3639778d93
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-20 19:55:13 -0700

    Update urls.py

--- syp/myapp/urls.py
+++ syp/myapp/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
-    path('base/',views.user_base,name='base')
+    path('base/',views.user_base,name='base'),
+    path('about/',views.user_about,name='about'),
+    path('shop/',views.user_shop,name='shop'),
+    path('feedback/',views.user_feedback,name='feedback'),
+    path('thank_you/',views.thank_you,name='thank_you'),
 ]
