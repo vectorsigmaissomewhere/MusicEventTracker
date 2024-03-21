8ae0127da7e72cb9c3c56df2c23237accfb2bac3
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-20 19:54:43 -0700

    Update models.py

--- syp/myapp/models.py
+++ syp/myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+#now we need user interaction as foreign key while sending feedback to admin
+from django.contrib.auth.models import User
+
 # Create your models here.
 class MusicalEvent(models.Model):
     artist = models.CharField(max_length=100)
@@ -11,3 +14,23 @@ class MusicalEvent(models.Model):
 
     def __str__(self):
         return self.artist + ' - ' + self.place + ' - ' + str(self.time)
+    
+
+class OurProduct(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_size = models.CharField(max_length=100)
+    product_quantity = models.IntegerField()
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.product_name
+    
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    feedback_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Feedback from {self.user.username if self.user else "Anonyous"}'
