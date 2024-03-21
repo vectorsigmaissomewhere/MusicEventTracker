4ebb3b7d41c505645968b14b744ffe35adec7c5c
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-20 19:54:18 -0700

    Update forms.py

--- syp/myapp/forms.py
+++ syp/myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Feedback
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -10,3 +11,12 @@ class SignupForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['feedback_text']
+        widgets = {
+            'feedback_text': forms.Textarea(attrs={'rows': 4, 'cols': 50, 'placeholder': 'Enter your feedback here...'})
+        }
